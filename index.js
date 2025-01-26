517cbfac246254681513ca9102df08ef2b18c072
Olena-Synytsia <alenasinitsa85@gmail.com>
2025-01-26 12:04:05 +0200

    3

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
-import initMongoConnection from './db/initMongoConnection.js';
-import setupServer from './server.js';
+import initMongoConnection from './src/db/initMongoConnection.js';
+import setupServer from './src/server.js';
 
 dotenv.config();
 
